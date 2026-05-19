92d0ed528c0b4edd5ae0ec0f1b44b7239e6ac29e
hoshikun1127 <111504546+hoshikun1127@users.noreply.github.com>
2026-05-19 14:34:53 +0900

    kinたま

--- calc.js
+++ calc.js
@@ -1,3 +1,12 @@
-function result() {
-  alert("JS動いた！");
+function check_result() {
+  // alert("JS動いた！");
+  let selected = document.querySelector('input[name = "sleep"]:checked')
+
+    if(selected) {
+        let value = selected.value
+        document.getElementById("result").textContent = "睡眠の点数は " + value + " です";
+    } else {
+
+        document.getElementById("result").textContent = "選択してください";
+    }
 }
